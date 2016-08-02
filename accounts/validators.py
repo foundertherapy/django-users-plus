d8d5a0493c5009fca39338f8d86a29917560a11f
nasief <nasief304@gmail.com>
2016-08-02 13:44:44 +0300

    Use single quotes

--- accounts/validators.py
+++ accounts/validators.py
@@ -5,7 +5,7 @@ import re
 class ComplexPasswordValidator(object):
 
     def validate(self, password, user=None):
-        regex = "(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]"
+        regex = '(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]'
         if not re.match(regex, password):
              raise ValidationError(_('Password should contain uppercase, lowercase and numeric values and at least'
                                      'one of the following $@$!%*?&'), code='password_is_weak')
