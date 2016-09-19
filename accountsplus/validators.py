804a381f4a3c85d259a316afd59eb2ee7be5554d
nasief <nasief304@gmail.com>
2016-09-19 17:31:57 +0300

    fix some issue in source code

--- accountsplus/validators.py
+++ accountsplus/validators.py
@@ -8,8 +8,8 @@ class ComplexPasswordValidator(object):
         regex = '(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@#!%*?&])[A-Za-z\d$@#!%*?&]'
         if not re.match(regex, password):
              raise ValidationError(_('Password should contain uppercase, lowercase, numeric values and at least '
-                                        + 'one of the following $@#!%*?&'), code='password_is_weak')
+                                     'one of the following $@#!%*?&'), code='password_is_weak')
 
     def get_help_text(self):
          return _('Password should contain uppercase, lowercase, numeric values and at least '
-                    + 'one of the following $@#!%*?&')
+                  'one of the following $@#!%*?&')
