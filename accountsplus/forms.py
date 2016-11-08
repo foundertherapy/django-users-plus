8aacb23cfd8526ace7e4b52edfa6647b43165fad
nasief <nasief304@gmail.com>
2016-11-08 15:57:06 +0200

    checking if user has a record associated with the email

--- accountsplus/forms.py
+++ accountsplus/forms.py
@@ -3,7 +3,16 @@ from __future__ import unicode_literals
 import django.forms
 from captcha.fields import ReCaptchaField
 
+import models
+
 
 class CaptchaForm(django.forms.Form):
     captcha = ReCaptchaField()
     username = django.forms.CharField()
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        exists = models.BaseUser.objects.filter(baseuser_email=username).exists()
+        if not exists:
+            raise django.forms.ValidationError("Username does not belong to a registered user")
+        return username
