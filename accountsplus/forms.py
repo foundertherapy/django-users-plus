cb1bf5709926515989e4d5e051f42ee4460be42e
nasief <nasief304@gmail.com>
2016-11-08 16:44:23 +0200

    support raising erros when email is not recorded

--- accountsplus/forms.py
+++ accountsplus/forms.py
@@ -1,8 +1,13 @@
 from __future__ import unicode_literals
 
+import importlib
+
 import django.forms
-from captcha.fields import ReCaptchaField
 from django.conf import settings
+from django.apps import apps
+
+from captcha.fields import ReCaptchaField
+
 
 class CaptchaForm(django.forms.Form):
     captcha = ReCaptchaField()
@@ -10,8 +15,8 @@ class CaptchaForm(django.forms.Form):
 
     def clean_username(self):
         username = self.cleaned_data.get("username")
-        User = type(getattr(settings, 'AUTH_USER_MODEL'))
-        exists = User.objects.filter(baseuser_email=username).exists()
+        User = apps.get_model(getattr(settings, 'AUTH_USER_MODEL'))
+        exists = User.objects.filter(email=username).exists()
         if not exists:
             raise django.forms.ValidationError("Username does not belong to a registered user")
         return username
