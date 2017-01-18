e14c16ddac8be8d5bd4c90f6fc9ae7cc5711fad0
nasief <nasief304@gmail.com>
2017-01-19 00:00:44 +0200

    an attempt to work with custom admin sites

--- accountsplus/apps.py
+++ accountsplus/apps.py
@@ -1,8 +1,14 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
+import django.contrib.admin
 
 
 class AccountsConfig(AppConfig):
     name = 'accountsplus'
     verbose_name = "Accounts Plus"
+
+    from accountsplus.forms import EmailBasedAdminAuthenticationForm
+
+    django.contrib.admin.autodiscover()
+    django.contrib.admin.site.login_form = EmailBasedAdminAuthenticationForm
