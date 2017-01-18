1c2393882370833b5e04248d1991b5d2bc314d94
nasief <nasief304@gmail.com>
2017-01-19 00:07:49 +0200

    Revert code

--- accountsplus/__init__.py
+++ accountsplus/__init__.py
@@ -1,9 +1,3 @@
-from accountsplus.forms import EmailBasedAdminAuthenticationForm
-import django.contrib.admin
-
 __version__ = '1.4.0'
 
 default_app_config = 'accountsplus.apps.AccountsConfig'
-
-django.contrib.admin.autodiscover()
-django.contrib.admin.site.login_form = EmailBasedAdminAuthenticationForm
